3770c5011179d18ace80494d8365c3513c970114
Joel-Baptista <joelbaptista@ua.pt>
2021-11-11 09:20:50 +0000

    Atualizacao

--- P6/Ex1/main.py
+++ P6/Ex1/main.py
@@ -3,5 +3,33 @@
 import cv2 as cv
 import numpy as np
 
-# Alinea a)
 
+def paint(event, x, y, flags, o):
+    pass
+
+
+def main():
+
+    global img
+
+    # Alinea a)
+    # img = cv.imread('atlascar.png', cv.IMREAD_COLOR)
+    # cv.circle(img, (img.shape[1]//2, img.shape[0]//2), 100, (0, 0, 255), -1)
+    # cv.imshow('Ex1a', img)  # Display the image
+    # cv.imshow('Ex1a_1', img)  # Display the image
+    #
+    # Alinea b)
+    # img = cv.imread('atlascar.png', cv.IMREAD_COLOR)
+    # img_text = cv.putText(img, 'PSR', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    # cv.imshow('Ex1b', img_text)  # Display the image
+
+    # Alinea c)
+
+    img = np.ones((600, 400))
+    cv.imshow('Ex1c', img)
+    cv.setMouseCallback('Ex1c', paint)
+    cv.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
